05d197a263c8a4a99897c30965c4989ed85040c9
jgoates1 <jgoates1@byu.edu>
2026-02-25 10:15:16 -0700

    homecontrlloer ish

--- Mission8/Controllers/HomeController.cs
+++ Mission8/Controllers/HomeController.cs
@@ -6,19 +6,67 @@ namespace Mission8.Controllers;
 
 public class HomeController : Controller
 {
+    private ITaskRepository _repo;
+
+    public HomeController(ITaskRepository temp)
+    {
+        _repo = temp;
+    }
+    
+    [HttpGet]
     public IActionResult Index()
     {
-        return View();
+        var tasks = _repo.Tasks
+            .Where(x => x.Completed == false)
+            .ToList();
+
+        return View(tasks);
+    }
+    
+    [HttpGet]
+    public IActionResult Add_Task()
+    {
+        ViewBag.Category = _repo.Category.ToList()
+        return View()
     }
 
-    public IActionResult Privacy()
+    [HttpPost]
+    public IActionResult Add_Task(TaskItem item)
     {
-        return View();
+        // use repository to add something to the database
+        if (ModelState.IsValid)
+        {
+            _repo.AddTask(item);
+            return RedirectToAction("Index");
+        }
+        
+        // if there are errors return this view with the errors
+        ViewBag.Category = _repo.Category.ToList();
+        return View(response);
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    [HttpGet]
+    public IActionResult Edit_Task(int id)
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        var tasktoEdit = _repo.TaskItem.Single(x => x.TaskId == id);
+        ViewBag.Category = _repo.Category.ToList();
+        return View("Add_Task", tasktoEdit);
     }
+
+    [HttpPost]
+    public IActionResult Edit_Task(TaskItem updatedTask)
+    {
+        _repo.UpdateTask(updatedTask);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult Delete_Task(int id)
+    {
+        var task = _repo.Tasks.Single(x => x.TaskId == id);
+        _repo.DeleteTask(task);
+    
+        return RedirectToAction("Index");
+    }
+    
 }
